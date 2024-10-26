b03abd2d22d912a694565bdeb0be1ca08fa7a1f0
waad <waadmahmoud2044@gmail.com>
2024-10-26 23:51:54 +0200

    homescreen done

---
+++ lib/edit_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class EditScreen extends StatelessWidget {
+  static const String routeName='edit';
+  const EditScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,38 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:editing_image/edit_screen.dart';
+import 'package:editing_image/home_screen.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+  runApp(
+    EasyLocalization(
+        supportedLocales: [
+          Locale('en'),
+          Locale('ar'),
+        ],
+        path: 'assets/translations',
+        saveLocale: true,
+        startLocale: Locale("en"),
+        child: MyApp()),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
+      debugShowCheckedModeBanner: false,
+      initialRoute: HomeScreen.routeName,
+      routes: {
+        HomeScreen.routeName: (context) => HomeScreen(),
+        EditScreen.routeName: (context) => EditScreen()
+      },
     );
   }
+}
